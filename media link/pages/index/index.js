55d1ec0e7bf8df3dd2b4f258d5d4bbf97f479215
Yroiluo <Yoriluo@163.com>
2018-05-18 14:07:23 +0800

    修改文字跑马灯效果

--- media link/pages/index/index.js
+++ media link/pages/index/index.js
@@ -5,32 +5,14 @@ const app = getApp()
 Page({
   data: {
     inviteDialog:false,
-    animation:true
+    left:0,
+    data:'新店开张，大酬宾，希望客户能够在本店享受到令人愉悦的服务店享受到令人愉悦的服务'
   },
 
   onShow: function () {
-    console.log(this.data);
-    setInterval(this.jud,6000);
+    this.animation();
   },
 
-  jud:function () {
-    console.log(this.data);
-    if (this.data.animation == true) {
-      this.setData({
-        animation:false
-      })
-      var vx = this;
-      setTimeout(function(){
-        vx.setData({
-          animation:true
-        })
-      },10)
-    } else {
-      this.setData({
-        animation:true
-      })
-    }
-  },
 
   showDialog:function() {
     this.setData({
@@ -53,5 +35,56 @@ Page({
         })
       }
     })
+  },
+
+  animation:function() {
+    var len = this.data.data.length * 20;
+    var time = len / 5;
+    this.run1(len,time);
+    var that = this;
+    var subLen = 530 + len;
+    var time2 = subLen / 5;
+    setTimeout(function() {
+      console.log('doSetTimeOut')
+      that.setData({
+        left: 530
+      })
+      setInterval(function() {
+        that.run2(that,len)
+      },50*(time2+2))
+    },50*(time+1))
+  },
+
+  run1:function(len,time) {
+    var that = this;
+    console.log(len);
+    var timer = setInterval(function(){
+      that.data.left -= 5;
+      that.setData({
+        left:that.data.left
+      })
+      if (-that.data.left>len) {
+        clearInterval(timer);
+      }
+    },50)
+  },
+
+  run2:function(that,len) {
+    var vm = that;
+    vm.setData({
+      left:530
+    })
+    var timer2 = setInterval(function() {
+      vm.data.left -=5;
+      vm.setData({
+        left: vm.data.left
+      })
+      if (-that.data.left > len) {
+        vm.setData({
+          left:530
+        });
+        clearInterval(timer2);
+      }
+    },50)
   }
  })
